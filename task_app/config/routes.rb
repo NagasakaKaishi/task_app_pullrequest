7159c3dbaf31592b409d590ee55f3f00f8d4769e
EC2 Default User <ec2-user@ip-172-31-45-101.ap-southeast-1.compute.internal>
2021-11-03 11:43:48 +0000

    developブランチでcommit、pushする

--- task_app/config/routes.rb
+++ task_app/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'schedules#new'
+  
+  root 'static_pages#home'
+  
+  # get '/home', to: 'static_pages#home'
+  
+  resources :schedules
 end
